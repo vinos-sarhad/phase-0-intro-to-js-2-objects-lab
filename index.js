a262b93b92087a09cf5fe3ad73e68a644d89b232
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-20 18:02:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+let employee={
+    name :"Sam",
+    streetAddress :"11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value // Update or add the specified key-value pair
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value;
+  
+     
+    return employee;
+  }
+  function  deleteFromEmployeeByKey(employee, key) {
+    // Create a shallow copy of the employee object using the spread operator
+    let clonedEmployee = { ...employee };
+  
+    // Delete the specified key from the cloned employee object
+    delete clonedEmployee[key];
+  
+    // Return the new employee object without the specified key
+    return clonedEmployee;
+  }
+
+ 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key from the original employee object
+    delete employee[key];
+  
+    // Return the modified employee object (although not necessary)
+    return employee;
+  }
